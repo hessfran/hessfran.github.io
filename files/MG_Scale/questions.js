c2cab5d82e822653e60fade9f207e54b028332d9
hessfran <49448459+hessfran@users.noreply.github.com>
2026-06-12 11:26:52 +0200

    Add files via upload

---
+++ files/MG_Scale/questions.js
@@ -0,0 +1,63 @@
+const SECTIONS = [
+    {
+        name: 'Ökologie',
+        questions: [
+            '01 Es werden keine <b>Tierversuche</b> durgeführt.',
+            '02 Es werden <i>überwiegend</i> (>50 %) <b>nachwachsende Rohstoffe</b> verwendet.',
+            '03 Das Produkt verursacht keine oder vernachlässigbare <b>Bodendegradierung</b>.',
+            '04 Das Produkt verursacht keine oder vernachlässigbare <b>regionale Austrocknung</b>.',
+            '05 Das Produkt verursacht keine oder vernachlässigbare <b>Umweltbelastung während der Nutzungsphase</b>.',
+            '06 Die Herstellung des Produkts trägt nicht oder vernachlässigbar zur <b>Luftverschmutzung</b> bei.',
+            '07 Es werden keine oder vernachlässigbare <b>giftige oder gefährliche Stoffe über Wasser verbreitet</b>.',
+            '08 Das <b>Abwasser</b> wird nicht in Gewässer eingeleitet.',
+            '09 Der <b>Abfall</b>, der bei Produktion und/oder Nutzung anfällt, ist <b>biologisch abbaubar</b>.',
+            '10 Es werden seitens des Herstellers inklusive Subunternehmen <i>ernsthafte Maßnahmen</i> zur <b>Reduzierung der allgemeinen Umweltbelastung</b> getroffen.',
+            '11 Die Rohstoffe stammen aus <b>ökologischem Anbau</b>.',
+            '12 Das Unternehmen beteiligt sich nicht an der <b>Abholzung von Wäldern</b> und bewirtschaftet Wälder nur nachhaltig.',
+            '13 Das Produkt beeinträchtigt die <b>biologische Vielfalt</b> nicht oder auf vernachlässigbare Weise.',
+            '14 Besteht die <b>Verpackung</b> <i>überwiegend</i> aus recycelten oder wiederverwendeten Materialien oder kann die Verpackung anderweitig genutzt werden, statt weggeworfen zu werden?',
+            '15 Die <b>Lieferkette</b> ist auf kurze Transportwege optimiert.'
+        ]
+    },
+    {
+        name: 'Soziales',
+        questions: [
+            '16 Entlang der <i>gesamten Lieferkette</i> werden <b>existenzsichernde Löhne</b> gezahlt, Subunternehmen eingeschlossen.',
+            '17 Alle Mitarbeitenden werden <b>gleich entlohnt</b> – unabhängig von Geschlecht, Alter oder Herkunft.',
+            '18 Es kommt entlang der Lieferkette nicht zu <b>Kinderarbeit</b>, Subunternehmen eingeschlossen.',
+            '19 Es gab in der Vergangenheit <b>keine dokumentierten Fälle von Kinderarbeit</b> entlang der Lieferkette, Subunternehmen eingeschlossen.',
+            '20 Mitarbeitende erfahren bei <b>psychischen und physischen Problemen Unterstützung</b> durch das Unternehmen.',
+            '21 Beschäftigte werden <b>in Entscheidungen einbezogen</b> (z. B. über Betriebsräte).',
+            '22 Es gibt es eine <b>Serviceanlaufstelle für Konsument*innen</b>.',
+            '23 Es werden <b>sichere Arbeitsbedingungen</b> entlang der gesamten Lieferkette gewährleistet.',
+            '24 <b>Gewerkschaften</b> sind erlaubt und es steht es den Mitarbeitenden frei sich in diesen zu engagieren und diesen beizutreten.',
+            '25 Es gibt eine <i>wirksame</i> <b>Richtlinie zur Achtung der Menschenrechte</b>.',
+            '26 Es gibt entlang der Lieferkette einen <b>Kündigungsschutz</b> für Arbeitnehmer*innen, inklusive Standorte der Subunternehmen.',
+            '27 Entlang der Lieferkette, inklusive Subunternehmen, herrschen <b>angemessene Arbeitsbedingungen</b> im Bezug auf Maximalarbeitszeit und Überstunden.',
+            '28 Es kam in der Vergangenheit <b>nicht zu Menschenrechtsverletzungen</b> im Umgang mit Mitarbeitenden.',
+            '29 Es gibt entlang der Lieferkette <b>faire arbeitsrechtliche Bestimmungen</b> (Urlaub, Krankheit, Mutterschutz etc.) und diese treffen auch auf Subunternehmen zu.',
+            '30 Es besteht eine interne <b>Beschwerdestelle für Menschenrechtsverletzungen</b> sowie Diskriminierung.',
+            '31 In Produktionsländern werden <b>lokale Initiativen und Bewegungen</b> einbezogen.'
+        ]
+    },
+    {
+        name: 'Ökonomie',
+        questions: [
+            '32 Das Unternehmen <b>informiert und sensibilisiert</b> seine Kund*innen zu Umweltthemen.',
+            '33 Die unterschiedlichen <b>Etappen der Lieferkette</b> werden so gewählt, dass möglichst nachhaltig und effizient produziert werden kann.',
+            '34 Es wird <b>kein Greenwashing</b> betrieben.',
+            '35 Die Firma <b>informiert über den verantwortungsvollen Konsum</b> ihrer Produkte.',
+            '36 Die Firma <b>investiert in Forschung</b>, um ihre Produkte nachhaltiger zu gestalten.',
+            '37 Die Firma nutzt <i>vertrauenswürdigei</i> <b>Nachhaltigkeitslabel</b>.',
+            '38 In der Produktion werden <i>überwiegend (>50%)</i> <b>erneuerbare Energien</b> genutzt.',
+            '39 <b>Wirtschaftliche Aspekte der Nachhaltigkeit</b> werden auf allen Etappen der Lieferkette (inkl. Subunternehmen) angewendet und kontrolliert.',
+            '40 Das Unternehmen <b>unterstützt nachhaltige Projekte</b> <i>wirkungsvoll</i>.',
+            '41 Bei der Auswahl der Materialien und Bestandteile des Produktes wird sichergestellt, dass diese einfach <b>reparierbar und recyclebar</b> sind.',
+            '42 Bei der Entwicklung von Produkten wird <b>Life Cycle Assessment</b> angewendet.',
+            '43 Die Firma stellt transparente <b>Informationen zu Themen der Nachhaltigkeit</b> für die Öffentlichkeit <i>transparent</i> zur Verfügung.',
+            '44 Das Unternehmen hat eine Nachhaltigkeitsstrategie mit konkreten Zielen.',
+            '45 Das Unternehmen betreibt <b>regelmäßiges Monitoring und Weiterentwicklung</b> der Nachhaltigkeitsstrategie?'
+            
+        ]
+    }
+];
